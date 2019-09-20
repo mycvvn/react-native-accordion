bde63af4403171e24703674478b3fd4031f8bbae
Sơn Ngọc <sondnpt00343@gmail.com>
2019-09-20 16:50:27 +0700

    Optimize with prettier

--- src/Panel.js
+++ src/Panel.js
@@ -3,31 +3,31 @@ import PropTypes from 'prop-types';
 import { View, ViewPropTypes } from 'react-native';
 
 function Panel(props) {
-    return (
-        <View style={[props.panelContainerStyle, props.containerStyle]}>
-            {props.children}
-        </View>
-    );
+  return (
+    <View style={[props.panelContainerStyle, props.containerStyle]}>
+      {props.children}
+    </View>
+  );
 }
 
-const defaultListener = () => {}
+const defaultListener = () => {};
 
 Panel.propTypes = {
-    title: PropTypes.string.isRequired,
-    onOpen: PropTypes.func,
-    onClose: PropTypes.func,
-    panelContainerStyle: ViewPropTypes.style,
-    containerStyle: ViewPropTypes.style,
-    children: PropTypes.node,
-}
+  title: PropTypes.string.isRequired,
+  onOpen: PropTypes.func,
+  onClose: PropTypes.func,
+  panelContainerStyle: ViewPropTypes.style,
+  containerStyle: ViewPropTypes.style,
+  children: PropTypes.node
+};
 
 Panel.defaultProps = {
-    title: undefined,
-    onOpen: defaultListener,
-    onClose: defaultListener,
-    panelContainerStyle: undefined,
-    containerStyle: undefined,
-    children: null,
-}
+  title: undefined,
+  onOpen: defaultListener,
+  onClose: defaultListener,
+  panelContainerStyle: undefined,
+  containerStyle: undefined,
+  children: null
+};
 
 export default Panel;
